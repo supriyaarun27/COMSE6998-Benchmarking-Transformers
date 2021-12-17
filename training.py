cd115d9568439fbbc8e8c0f3e0bdcb9982a58f12
supriyaarun27 <64113420+supriyaarun27@users.noreply.github.com>
2021-12-17 18:46:39 -0500

    Add training code

---
+++ training.py
@@ -0,0 +1,218 @@
+squad_v2 = False
+model_checkpoint = "distilbert-base-uncased"
+batch_size = 16
+
+# import dependencies 
+from datasets import load_dataset, load_metric
+from transformers import AutoTokenizer
+from datasets import ClassLabel, Sequence
+import random
+import pandas as pd
+from IPython.display import display, HTML
+import transformers
+from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
+from transformers import default_data_collator
+
+datasets = load_dataset("squad_v2" if squad_v2 else "squad")
+
+
+def show_random_elements(dataset, num_examples=10):
+    assert num_examples <= len(dataset), "Can't pick more elements than there are in the dataset."
+    picks = []
+    for _ in range(num_examples):
+        pick = random.randint(0, len(dataset)-1)
+        while pick in picks:
+            pick = random.randint(0, len(dataset)-1)
+        picks.append(pick)
+    
+    df = pd.DataFrame(dataset[picks])
+    for column, typ in dataset.features.items():
+        if isinstance(typ, ClassLabel):
+            df[column] = df[column].transform(lambda i: typ.names[i])
+        elif isinstance(typ, Sequence) and isinstance(typ.feature, ClassLabel):
+            df[column] = df[column].transform(lambda x: [typ.feature.names[i] for i in x])
+    display(HTML(df.to_html()))
+
+show_random_elements(datasets["train"])
+    
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+assert isinstance(tokenizer, transformers.PreTrainedTokenizerFast)
+max_length = 384 # The maximum length of a feature (question and context)
+doc_stride = 128 # The authorized overlap between two part of the context when splitting it is needed.
+pad_on_right = tokenizer.padding_side == "right"
+
+for i, example in enumerate(datasets["train"]):
+    if len(tokenizer(example["question"], example["context"])["input_ids"]) > 384:
+        break
+example = datasets["train"][i]
+
+tokenized_example = tokenizer(
+    example["question"],
+    example["context"],
+    max_length=max_length,
+    truncation="only_second",
+    return_overflowing_tokens=True,
+    stride=doc_stride
+)
+
+tokenized_example = tokenizer(
+    example["question"],
+    example["context"],
+    max_length=max_length,
+    truncation="only_second",
+    return_overflowing_tokens=True,
+    return_offsets_mapping=True,
+    stride=doc_stride
+)
+print(tokenized_example["offset_mapping"][0][:100])
+
+first_token_id = tokenized_example["input_ids"][0][1]
+offsets = tokenized_example["offset_mapping"][0][1]
+print(tokenizer.convert_ids_to_tokens([first_token_id])[0], example["question"][offsets[0]:offsets[1]])
+
+sequence_ids = tokenized_example.sequence_ids()
+print(sequence_ids)
+
+answers = example["answers"]
+start_char = answers["answer_start"][0]
+end_char = start_char + len(answers["text"][0])
+
+# Start token index of the current span in the text.
+token_start_index = 0
+while sequence_ids[token_start_index] != 1:
+    token_start_index += 1
+
+# End token index of the current span in the text.
+token_end_index = len(tokenized_example["input_ids"][0]) - 1
+while sequence_ids[token_end_index] != 1:
+    token_end_index -= 1
+
+# Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
+offsets = tokenized_example["offset_mapping"][0]
+if (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
+    # Move the token_start_index and token_end_index to the two ends of the answer.
+    # Note: we could go after the last offset if the answer is the last word (edge case).
+    while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
+        token_start_index += 1
+    start_position = token_start_index - 1
+    while offsets[token_end_index][1] >= end_char:
+        token_end_index -= 1
+    end_position = token_end_index + 1
+    print(start_position, end_position)
+else:
+    print("The answer is not in this feature.")
+
+
+
+def prepare_train_features(examples):
+    # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+    # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+    # left whitespace
+    examples["question"] = [q.lstrip() for q in examples["question"]]
+
+    # Tokenize our examples with truncation and padding, but keep the overflows using a stride. This results
+    # in one example possible giving several features when a context is long, each of those features having a
+    # context that overlaps a bit the context of the previous feature.
+    tokenized_examples = tokenizer(
+        examples["question" if pad_on_right else "context"],
+        examples["context" if pad_on_right else "question"],
+        truncation="only_second" if pad_on_right else "only_first",
+        max_length=max_length,
+        stride=doc_stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    # Since one example might give us several features if it has a long context, we need a map from a feature to
+    # its corresponding example. This key gives us just that.
+    sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+    # The offset mappings will give us a map from token to character position in the original context. This will
+    # help us compute the start_positions and end_positions.
+    offset_mapping = tokenized_examples.pop("offset_mapping")
+
+    # Let's label those examples!
+    tokenized_examples["start_positions"] = []
+    tokenized_examples["end_positions"] = []
+
+    for i, offsets in enumerate(offset_mapping):
+        # We will label impossible answers with the index of the CLS token.
+        input_ids = tokenized_examples["input_ids"][i]
+        cls_index = input_ids.index(tokenizer.cls_token_id)
+
+        # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+        sequence_ids = tokenized_examples.sequence_ids(i)
+
+        # One example can give several spans, this is the index of the example containing this span of text.
+        sample_index = sample_mapping[i]
+        answers = examples["answers"][sample_index]
+        # If no answers are given, set the cls_index as answer.
+        if len(answers["answer_start"]) == 0:
+            tokenized_examples["start_positions"].append(cls_index)
+            tokenized_examples["end_positions"].append(cls_index)
+        else:
+            # Start/end character index of the answer in the text.
+            start_char = answers["answer_start"][0]
+            end_char = start_char + len(answers["text"][0])
+
+            # Start token index of the current span in the text.
+            token_start_index = 0
+            while sequence_ids[token_start_index] != (1 if pad_on_right else 0):
+                token_start_index += 1
+
+            # End token index of the current span in the text.
+            token_end_index = len(input_ids) - 1
+            while sequence_ids[token_end_index] != (1 if pad_on_right else 0):
+                token_end_index -= 1
+
+            # Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
+            if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
+                tokenized_examples["start_positions"].append(cls_index)
+                tokenized_examples["end_positions"].append(cls_index)
+            else:
+                # Otherwise move the token_start_index and token_end_index to the two ends of the answer.
+                # Note: we could go after the last offset if the answer is the last word (edge case).
+                while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
+                    token_start_index += 1
+                tokenized_examples["start_positions"].append(token_start_index - 1)
+                while offsets[token_end_index][1] >= end_char:
+                    token_end_index -= 1
+                tokenized_examples["end_positions"].append(token_end_index + 1)
+
+    return tokenized_examples
+
+
+features = prepare_train_features(datasets['train'][:5])
+tokenized_datasets = datasets.map(prepare_train_features, batched=True, remove_columns=datasets["train"].column_names)
+
+#fine tuning model 
+model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
+model_name = model_checkpoint.split("/")[-1]
+args = TrainingArguments(
+    f"{model_name}-finetuned-squad",
+    evaluation_strategy = "epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    push_to_hub=True,
+)
+
+data_collator = default_data_collator
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+#train model 
+trainer.train()
+
+#save model and push to hub
+trainer.save_model("squeezebert-squad-trained")
+trainer.push_to_hub("squeezebert-squad-trained")
